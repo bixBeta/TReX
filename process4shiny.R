6d989d4a7617548630ed3afdc4b8ce624155458b
BiX <33409348+bixBeta@users.noreply.github.com>
2019-04-10 22:20:31 -0400

    Update process4shiny.R

--- process4shiny.R
+++ process4shiny.R
@@ -12,10 +12,10 @@ dds <- DESeq(dds)
 ## -------------------------------------------------------------------------------------------------------------------
 # add .custom to all contrast objects 
 
-cColdHot.custom <- results(dds, contrast=c("group", "cHOT", "cCOLD"), alpha = 0.05)      
-pColdHot.custom <- results(dds, contrast=c("group", "pHOT", "pCOLD"), alpha = 0.05)
-cpHot.custom <- results(dds, contrast=c("group", "cHOT", "pHOT"), alpha = 0.05)
-cpCold.custom <- results(dds, contrast=c("group", "cCOLD", "pCOLD"), alpha = 0.05)
+cHotvscCold.custom <- results(dds, contrast=c("group", "cHOT", "cCOLD"), alpha = 0.05)      
+pHotvspCold.custom <- results(dds, contrast=c("group", "pHOT", "pCOLD"), alpha = 0.05)
+cHotvspHot.custom <- results(dds, contrast=c("group", "cHOT", "pHOT"), alpha = 0.05)
+cColdvspCold.custom <- results(dds, contrast=c("group", "cCOLD", "pCOLD"), alpha = 0.05)
 
 pattern1 <- objects(pattern = ".custom")
 
@@ -76,3 +76,35 @@ for (i in 1:length(pattern3)) {
                           select(Id, matches("norm"), everything())), 
          envir = .GlobalEnv )
  }
+
+
+obj <- objects(pattern = ".final")
+
+for (i in 1:length(obj)) {
+  assign(x = paste0(obj[i],".Annotated"),
+         value = as.data.frame(mget(obj[i]),col.names = NULL) %>% 
+           rename(!!paste0(strsplit(obj[i], "\\.")[[1]][1],".baseMean") := baseMean,
+                  !!paste0(strsplit(obj[i], "\\.")[[1]][1],".log2FoldChange") := log2FoldChange,
+                  !!paste0(strsplit(obj[i], "\\.")[[1]][1],".stat") := stat,
+                  !!paste0(strsplit(obj[i], "\\.")[[1]][1],".pvalue") := pvalue,
+                  !!paste0(strsplit(obj[i], "\\.")[[1]][1],".lfcSE") := lfcSE,
+                  !!paste0(strsplit(obj[i], "\\.")[[1]][1],".padj") := padj),
+         envir = .GlobalEnv
+         
+  )
+}
+
+obj2 <- objects(pattern = "Annotated")
+
+new.data.frame <- as.data.frame(mget(obj2[1]),col.names = NULL)
+
+for (i in 2:length(obj2)){
+  
+  new.data.frame <- full_join(new.data.frame, 
+                              select(as.data.frame(mget(obj2[i]),col.names = NULL), Id, matches("vs")), 
+                              by = "Id")
+  
+}
+
+write.table(new.data.frame, "final.txt", sep = "\t", quote = F, row.names = F)
+
