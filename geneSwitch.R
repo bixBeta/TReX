8c644abb63c686dc243f825c68f957924dfe4673
BiX <33409348+bixBeta@users.noreply.github.com>
2019-04-12 15:19:10 -0400

    Update geneSwitch.R

--- geneSwitch.R
+++ geneSwitch.R
@@ -1,22 +1,25 @@
 #!/usr/bin/env Rscript
 
-suppressWarnings(library("dplyr"))
+suppressPackageStartupMessages(library("dplyr"))
 
-names 	<- list.files(getwd(), pattern = ".rawCounts" )
-gff 	<- "/Users/fa286/bin/gff.Hash.Aegl5.0.txt" #hard path
+args <-  commandArgs(trailingOnly = T)
+gff <- args[1] # path for hash/dictionary file
+
+names   <- list.files(getwd(), pattern = ".rawCounts" )
+#gff    <- "/home/fa286/bin/scripts/gff.Hash.Aegl5.0.txt"
 
 swapFunc <- function(gff,countFile,names){
 
-	gff.mosquito <- read.table(gff, header = F)
-  	one <- read.table(countFile, header = F)
-  	swap <- left_join(one, gff.mosquito, by = c("V1" = "V1"))
-  	finalCount <- swap[,c(3,2)]
-  	write.table(finalCount, paste0(names,".swapCount"), quote = F, sep = "\t", col.names = F, row.names = F)
+        gff.mosquito <- read.table(gff, header = F)
+        one <- read.table(countFile, header = F)
+        swap <- left_join(one, gff.mosquito, by = c("V1" = "V1"))
+        finalCount <- swap[,c(3,2)]
+        write.table(finalCount, paste0(names,".swapCount"), quote = F, sep = "\t", col.names = F, row.names = F)
 
 }
 
 for (i in 1:length(names)){
 
-  	swapFunc(gff = gff, countFile = names[i], names = names[i] )
+        swapFunc(gff = gff, countFile = names[i], names = names[i] )
 
 }
