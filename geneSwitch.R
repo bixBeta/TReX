d7c6c83d1920bf955815b6765fe40319f9da8d26
BiX <33409348+bixBeta@users.noreply.github.com>
2019-04-23 15:50:38 -0400

    Update geneSwitch.R

--- geneSwitch.R
+++ geneSwitch.R
@@ -10,9 +10,9 @@ names   <- list.files(getwd(), pattern = ".rawCounts" )
 
 swapFunc <- function(gff,countFile,names){
 
-        gff.mosquito <- read.table(gff, header = F)
+        gff.custom <- read.table(gff, header = F)
         one <- read.table(countFile, header = F)
-        swap <- left_join(one, gff.mosquito, by = c("V1" = "V1"))
+        swap <- left_join(one, gff.custom, by = c("V1" = "V1"))
         finalCount <- swap[,c(3,2)]
         write.table(finalCount, paste0(names,".swapCount"), quote = F, sep = "\t", col.names = F, row.names = F)
 
