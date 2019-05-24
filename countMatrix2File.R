67ca89cd6f65565f521cf1a1a650c84556829a54
BiX <33409348+bixBeta@users.noreply.github.com>
2019-05-24 15:46:22 -0400

    Update countMatrix2File.R

--- countMatrix2File.R
+++ countMatrix2File.R
@@ -1,36 +1,44 @@
 library(dplyr)
 
-keys <- as.data.frame(t(phenoData))
-colnames(keys) <- c("file", "group")
+# keys <- as.data.frame(t(phenoData))
+# colnames(keys) <- c("file", "group")
 
+# arg <-  commandArgs(trailingOnly = T)
+# counts <- read.table(arg[1], header = T, sep = "\t")
+
+counts <- read.table("CPM_run1.txt", header = T, sep = "\t")
 
 counts <- counts %>%
-          mutate_at(1:24, ~ round(.,0))
+           mutate_at(2:ncol(counts), ~ round(.,0))
+ 
+row.names(counts) <- counts$X
 
-for (i in 1:24) {
-  
+counts <- counts %>% 
+  select(-1)
+
+for (i in 1:ncol(counts)) {
+
   assign(as.character(colnames(counts)[i]),
-         value = counts %>% 
+         value = counts %>%
                   select(colnames(counts)[i]) ,
          envir = .GlobalEnv)
 }
 
-# custom 
 hds <- objects(pattern = "HD")
 ncs <- objects(pattern = "NC")
 
 for (i in 1:length(hds)) {
-  
-  write.table(mget(hds)[i], file = paste0(hds[i], ".txt"), 
+
+  write.table(mget(hds)[i], file = paste0(hds[i], ".txt"),
               sep = "\t", quote = F, col.names = F)
-  
+
 }
 
 for (i in 1:length(ncs)) {
-  
-  write.table(mget(ncs)[i], file = paste0(ncs[i], ".txt"), 
+
+  write.table(mget(ncs)[i], file = paste0(ncs[i], ".txt"),
               sep = "\t", quote = F, col.names = F)
-  
+
 }
 
 
