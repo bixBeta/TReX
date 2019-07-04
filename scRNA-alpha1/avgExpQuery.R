db61654ddcb2c84ca2d62069bf268346c316e383
BiX <33409348+bixBeta@users.noreply.github.com>
2019-07-04 00:15:22 -0400

    Create avgExpQuery.R

---
+++ scRNA-alpha1/avgExpQuery.R
@@ -0,0 +1,12 @@
+geneExpression <- function(gene){
+  
+  queryExpression <- norm_1.1[rownames(norm_1.1) == gene, ]
+  query <- as.data.frame(t(queryExpression))
+  query$UMI <- rownames(query)
+  jnd <- left_join(myMeta, query, by = "UMI")
+  jnd <- jnd %>% 
+    group_by(Phenotype, CPET.Day) %>%
+    summarise_each(funs = (mean), !!ss)
+  print(jnd)
+}
+geneExpression("GRASP")
